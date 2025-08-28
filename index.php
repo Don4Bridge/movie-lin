4a0a7d6d646c19abc22fb4474a2f499b181a5d04
Don4Bridge <77544197+Don4Bridge@users.noreply.github.com>
2025-08-28 12:41:06 -0700

    Update index.php

--- index.php
+++ index.php
@@ -143,7 +143,7 @@ function convert_lin_to_pbn($lin) {
         $openingLeader = $seatOrder[$leaderIndex];
     }
 
-    $names = extract_names_from_lin($lin);
+    $names = extract_names_from_lin($normalizedLin);
 
     $pbn = "[Event \"BBO Movie\"]\n";
     $pbn .= "[Site \"Bridge Base Online\"]\n";
