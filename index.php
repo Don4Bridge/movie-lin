02de051e816ec14eefb4dc07d8d685212333a7ab
Don4Bridge <77544197+Don4Bridge@users.noreply.github.com>
2025-08-28 12:09:36 -0700

    Update index.php

--- index.php
+++ index.php
@@ -55,18 +55,8 @@ function convert_lin_to_pbn($lin) {
 foreach ($lines as &$segment) {
     $segment = str_replace('+', ' ', $segment);
 }
-$names = ['North' => '', 'East' => '', 'South' => '', 'West' => ''];
-foreach ($lines as $i => $tag) {
-    if ($tag === 'pn') {
-        $rawNames = explode('^', $lines[$i + 1] ?? '');
-        if (count($rawNames) === 4) {
-            $names = [
-                'North' => $rawNames[0],
-                'East'  => $rawNames[1],
-                'South' => $rawNames[2],
-                'West'  => $rawNames[3],
-            ];
-        }
+list($normalized, $boardId) = normalize_lin($lin);
+$names = extract_names_from_lin($normalized);
         break;
     }
 }
